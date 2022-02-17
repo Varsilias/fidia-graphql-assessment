cecd6283c86d7b1d8f12f8a0422c98c98a1d99eb
danielokoronkwo-coder <danielokoronkwo90@gmail.com>
2022-02-17 11:53:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Fidia Backend Task Solution
+
+This repository contains the code to the solution for the backend engineering assessment by [Fidia]('https://getfidia.com/)
+
+### Project Setup
+
+- Clone this project by running the command ```git clone```
---
+++ index.js
@@ -0,0 +1,66 @@
+import { ApolloServer, gql } from "apollo-server-express"
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core"
+import express from "express"
+import mongoose from "mongoose"
+import http from 'http'
+import 'dotenv/config'
+
+
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const startServer = async (typeDefs, resolvers) => {
+  const app = express()
+
+  try {
+    await mongoose.connect(process.env.DATABASE_URL)
+    console.log('mongo connected')
+  } catch (error) {
+    console.log({ message: error.message })
+  }
+
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+
+startServer(typeDefs, resolvers)
